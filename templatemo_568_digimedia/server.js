a96faaf6220354d2fc2142d29f221876b2cecd29
Shreyash Patel <65528297+Shreyashpatel2701@users.noreply.github.com>
2023-02-14 11:20:53 +0530

    beforebackend

---
+++ templatemo_568_digimedia/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = 5000;
+
+app.get('/',(req, res)=>{
+ res.send("hello!!")
+})
+
+
+
+app.listen(PORT, ()=>{
+    
+    console.log('server running at 5000')
+
+})
